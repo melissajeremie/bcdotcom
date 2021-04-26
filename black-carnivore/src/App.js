7d9cdda2cbc8fc9ea2f1d2eeaa8da4235092841d
Melissa Jeremie <melissajeremie@gmail.com>
2021-04-26 10:08:11 -0400

    initial react app

---
+++ black-carnivore/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, nice to meat you</h1>
+    </div>
+  );
+}
+
+export default App;
