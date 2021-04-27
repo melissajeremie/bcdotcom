a65c73454cb97d0450786492feb92f10460b547a
Melissa Jeremie <melissajeremie@gmail.com>
2021-04-27 12:56:01 -0400

    added router

--- black-carnivore/src/App.js
+++ black-carnivore/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, nice to meat you</h1>
+      <h1>Black Carnivore</h1>
     </div>
   );
 }
