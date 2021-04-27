df7141aec0b038b98dad11b44e20fe882bca79da
Melissa Jeremie <melissajeremie@gmail.com>
2021-04-27 15:24:53 -0400

    homepage video embed

--- black-carnivore/src/App.js
+++ black-carnivore/src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 class App extends React.Component {
   render() {
   return (
-    <div id="App">
-      <Header/>
-      <Nav/>
-      <Video/>
+    <div className="App">
+      <div id = "container">
+        <Header/>
+        <Nav/>
+        <Video/>
+      </div>
     </div>
   );
 }
@@ -18,6 +20,7 @@ class Header extends React.Component {
   return (
     <div id="header">
       <h1>Black Carnivore</h1>
+      <h3>A whole foods, meat-based lifestyle for vibrant health</h3>
     </div>
   );
 }
@@ -27,7 +30,14 @@ class Nav extends React.Component {
   render() {
   return (
     <div id="nav">
-      
+      <ul>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Why Carnivore?</a></li>
+        <li><a href="#">Podcast</a></li>
+        <li><a href="#">Shopping</a></li>
+        <li><a href="#">Merchandise</a></li>
+        <li><a href="#">Events</a></li>
+      </ul>
     </div>
   );
 }
@@ -37,7 +47,12 @@ class Video extends React.Component {
   render() {
   return (
     <div id="video">
-      
+      <iframe 
+      width="750" 
+      height="480" 
+      src="https://www.youtube.com/embed/VeWbhUFdtrY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+
+      </iframe>
     </div>
   );
 }
